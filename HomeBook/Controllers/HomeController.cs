866fed84a3ac264d2b89b4d945d064111f0e7d57
Simon Goodman <simon.goodman84@googlemail.com>
2016-06-12 13:49:01 +0100

    Add file upload

--- HomeBook/Controllers/HomeController.cs
+++ HomeBook/Controllers/HomeController.cs
@@ -1,4 +1,6 @@
-﻿using System.Web.Mvc;
+﻿using System.IO;
+using System.Web;
+using System.Web.Mvc;
 
 namespace HomeBook.Controllers
 {
@@ -7,6 +9,23 @@ namespace HomeBook.Controllers
         public ActionResult Index()
         {
             return View();
+        }
+
+        // This action handles the form POST and the upload
+        [HttpPost]
+        public ActionResult Index(HttpPostedFileBase file)
+        {
+            // Verify that the user selected a file
+            if (file != null && file.ContentLength > 0)
+            {
+                // extract only the filename
+                var fileName = Path.GetFileName(file.FileName);
+                // store the file inside ~/App_Data/uploads folder
+                var path = Path.Combine(Server.MapPath("~/App_Data/uploads"), fileName);
+                file.SaveAs(path);
+            }
+            // redirect back to the index action to show the form once again
+            return RedirectToAction("Index");
         }
     }
 }
